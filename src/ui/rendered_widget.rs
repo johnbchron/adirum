780ed4918727ebe3b6f049471299321e91a119b8
John Lewis <github@jlewis.sh>
2025-01-03 10:16:49 -0600

    feat: simplify `RenderedWidget`

--- src/ui/rendered_widget.rs
+++ src/ui/rendered_widget.rs
@@ -24,39 +24,30 @@ impl StatefulWidget for RenderedWidget {
 
   fn render(
     self,
-    mut target_area: Rect,
+    target_area: Rect,
     buf: &mut Buffer,
     state: &mut Self::State,
   ) {
     // make sure that the next render is synced to the current size
     state.last_area = target_area;
 
-    // keep the original position of the target area and zero it out
-    let target_area_original_x = target_area.x;
-    let target_area_original_y = target_area.y;
-    target_area.x = 0;
-    target_area.y = 0;
-    // also zero out the position of the buffer area
-    state.buffer.area.x = 0;
-    state.buffer.area.y = 0;
-
-    // get the intersection of the target area and the buffer area
-    let intersection = target_area.intersection(state.buffer.area);
-
-    // resize the render buffer to the intersection
-    state.buffer.resize(intersection);
-
-    // add back the original position to the target area
-    target_area.x = target_area_original_x;
-    target_area.y = target_area_original_y;
-
     // center the render buffer in the target area
-    let x_offset = (target_area.width - state.buffer.area.width) / 2;
-    let y_offset = (target_area.height - state.buffer.area.height) / 2;
+    let x_offset = ((target_area.width as i32 - state.buffer.area.width as i32)
+      / 2)
+      .max(0) as u16;
+    let y_offset = ((target_area.height as i32
+      - state.buffer.area.height as i32)
+      / 2)
+      .max(0) as u16;
     state.buffer.area.x = target_area.x + x_offset;
     state.buffer.area.y = target_area.y + y_offset;
 
-    // render the buffer to the main buffer
+    // resize the render buffer to the intersection of itself and the target
+    state
+      .buffer
+      .resize(target_area.intersection(state.buffer.area));
+
+    // copy the render buffer to the main buffer
     buf.merge(&state.buffer);
   }
 }
