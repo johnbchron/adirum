cfb1ec48ede69e7a33f52c3ea64ae08a7a9cf24e
John Lewis <github@jlewis.sh>
2025-03-16 21:38:18 +0100

    feat(render): add debug asserts for ProjectedPoint

--- crates/render/src/shapes/projected_point.rs
+++ crates/render/src/shapes/projected_point.rs
@@ -5,11 +5,17 @@ pub struct ProjectedPoint(IVec2, f32);
 
 impl ProjectedPoint {
   pub fn new(pos: IVec2, depth: f32) -> ProjectedPoint {
+    debug_assert!(depth - f32::EPSILON <= 1.0);
+    debug_assert!(depth + f32::EPSILON >= 0.0);
     ProjectedPoint(pos, depth)
   }
 
   pub fn pos(&self) -> IVec2 { self.0 }
   pub fn depth(&self) -> f32 { self.1 }
 
-  pub fn set_depth(&mut self, new: f32) { self.1 = new; }
+  pub fn set_depth(&mut self, new: f32) {
+    debug_assert!(new - f32::EPSILON <= 1.0);
+    debug_assert!(new + f32::EPSILON >= 0.0);
+    self.1 = new;
+  }
 }
